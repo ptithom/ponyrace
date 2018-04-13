bc5a68d371af647bcd72d2021b75389d6db630dc
Thomas sire <thomas.sire@komunityweb.com>
2018-04-13 14:54:19 +0200

    Router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-
+import { RouterModule } from '@angular/router';
+import {ROUTES} from './app.routes';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
@@ -10,7 +12,7 @@ import { RaceService } from './race.service';
 import { RaceComponent } from './race/race.component';
 import { PonyComponent } from './pony/pony.component';
 import { FromNowPipe } from './from-now.pipe';
-
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
@@ -20,9 +22,13 @@ import { FromNowPipe } from './from-now.pipe';
     RaceComponent,
     PonyComponent,
     FromNowPipe,
+    HomeComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(ROUTES),
+    FormsModule
   ],
   providers: [RaceService],
   bootstrap: [AppComponent]
